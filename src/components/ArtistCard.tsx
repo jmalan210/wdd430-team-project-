ca0eb5a5a1a86710e35275e817dd2e077d5cc27e
jmalan210 <>
2026-06-03 23:17:32 -0600

    more css tweaks

--- src/components/ArtistCard.tsx
+++ src/components/ArtistCard.tsx
@@ -5,14 +5,14 @@ export default function ArtistCard({artist}: any) {
     return (
             <Link href={`storefront/${artist.id}`} className="block h-full" >
             <div className="h-full flex flex-col border rounded-xl p-4 shadow-lg bg-ivory">
-                <div className="flex justify-start">
+                <div className="flex justify-between">
                     <div className="relative w-36 h-36 shrink-0">
                         <Image src={artist.image_url}
                             alt={`${artist.first_name} ${artist.last_name}`}
                             fill
                             className="object-cover rounded-lg shadow-lg border-4 border-white" />
                     </div>    
-                <div className="flex flex-col pl-3 justify-center">
+                <div className="flex flex-col pl-3 justify-center text-right">
                         <h2 className="text-2xl font-bold text-navy">{artist.first_name} {artist.last_name}</h2>
                         <h3 className="text-lg font-semibold text-terracotta">{artist.business_name}</h3>
                         <h4 className="text-sm italic text-sage">{artist.medium}</h4>
