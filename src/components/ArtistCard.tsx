cfb94cf981310f921878f0a64ba6fd30b07503f3
jmalan210 <>
2026-06-04 09:08:01 -0600

    css tweaks to the product cards

--- src/components/ArtistCard.tsx
+++ src/components/ArtistCard.tsx
@@ -6,17 +6,19 @@ export default function ArtistCard({artist}: any) {
             <Link href={`storefront/${artist.id}`} className="block h-full" >
             <div className="h-full flex flex-col border rounded-xl p-4 shadow-lg bg-ivory">
                 <div className="flex justify-between">
-                    <div className="relative w-36 h-36 shrink-0">
+                   
+                <div className="flex flex-col pl-3 justify-center text-left">
+                        <h2 className="text-2xl font-bold text-navy">{artist.first_name} {artist.last_name}</h2>
+                        <h3 className="text-lg font-semibold text-terracotta">{artist.business_name}</h3>
+                        <h4 className="text-sm italic text-sage">{artist.medium}</h4>
+                    </div>
+
+                     <div className="relative w-36 h-36 shrink-0">
                         <Image src={artist.image_url}
                             alt={`${artist.first_name} ${artist.last_name}`}
                             fill
                             className="object-cover rounded-lg shadow-lg border-4 border-white" />
                     </div>    
-                <div className="flex flex-col pl-3 justify-center text-right">
-                        <h2 className="text-2xl font-bold text-navy">{artist.first_name} {artist.last_name}</h2>
-                        <h3 className="text-lg font-semibold text-terracotta">{artist.business_name}</h3>
-                        <h4 className="text-sm italic text-sage">{artist.medium}</h4>
-                    </div>
                     
                     </div>
                 <div className="mt-4">
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -19,25 +19,30 @@ type Props = {
 export default function ProductCard({ product }: Props) {
     return (
         <div className="flex flex-col shadow-xl rounded-lg bg-ivory p-4">
+       
+            <div >
             <h3 className="text-2xl bold">{product.name}</h3>
             <p className="text-terracotta pb-2">by {product.business_name}</p>
             <p className="italic pb-2">${product.price}</p>
+            </div>
             
-            
-            <div className="flex gap-2 overflow-x-auto align-middle justify-center">
-                <p className="justify-center align-middle">{product.description}</p>
-                {product.images?.map((url: string, index: number) => (
+            <div className="relative h-72 w-full">
+                 {product.images?.map((url: string, index: number) => (
                     <Image
                         key={index}
                         src={`/${url}`}
                         alt={product.name}
-                        width={200}
-                        height={200}
-                        className="rounded-lg shadow-lg border-8 border-white"
+                        fill
+                        className="object-cover rounded-lg"
                     />
                 ))}
-                
+            </div>
+            
+            <div className="flex gap-2 overflow-x-auto align-middle justify-center">
+                <p className="justify-center align-middle line-clamp-3">{product.description}</p>
+               
             </div>
         </div>
+        
     );
 }
